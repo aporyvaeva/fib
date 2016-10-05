6830c88bac32278c46b4ff7d053b0983e5cfe8de
Angelina <angelinaporyvaeva0303@gmail.com>
2016-10-05 17:43:10 +0000

    changes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function fib(n){
 	var a = 1;
 	var b = 1;
-	for(var i = 3;i<n;i++){
+	if(n>2){
+	for(var i = 3;i<=n;i++){
 		var c = a+b;
 		a=b;
 		b=c;
 	}
+	}else return 1
 	return c;
 }
 function res(){

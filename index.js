fa203ad69d2a499eb5bf4159f37fe385e9449635
Angelina <angelinaporyvaeva0303@gmail.com>
2016-10-05 16:44:46 +0000

    new

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// JavaScript File
+function fib(n) {
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+function res(){
+document.getElementById("result").innerHTML="result:" + fib(document.getElementById("num").value);
+}
